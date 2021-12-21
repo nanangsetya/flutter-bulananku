5dfb7f5c90fbe2752fb565d0df95aa1af4111516
nanangsetya <nsbbusiness01@gmail.com>
2021-12-21 22:29:54 +0700

    change card month to gridview

--- lib/widget/dashboard_card_month.dart
+++ lib/widget/dashboard_card_month.dart
@@ -12,223 +12,201 @@ class _CardMonthState extends State<CardMonth> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                flex: 5,
-                child: Container(
-                  margin: EdgeInsets.only(right: 5, bottom: 5),
-                  padding: EdgeInsets.all(10),
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color(0xff161637),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                            color: Color(0xffECE445),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Icon(
-                          FontAwesomeIcons.utensils,
-                          color: Color(0xff161637),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10),
-                        child: Text(
-                          "Food",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 5),
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "125.000",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+        width: MediaQuery.of(context).size.width,
+        child: GridView.count(
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+          childAspectRatio: 16 / 9,
+          primary: false,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Color(0xff161637),
               ),
-              Expanded(
-                flex: 5,
-                child: Container(
-                  margin: EdgeInsets.only(left: 5, bottom: 5),
-                  padding: EdgeInsets.all(10),
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color(0xff161637),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Color(0xffECE445),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Icon(
+                      FontAwesomeIcons.utensils,
+                      color: Color(0xff161637),
+                    ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                            color: Color(0xff459CEE),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Icon(
-                          FontAwesomeIcons.tshirt,
-                          color: Color(0xff161637),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10),
-                        child: Text(
-                          "Cloth",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 5),
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "165.000",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                      )
-                    ],
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Food",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.normal),
+                    ),
                   ),
-                ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "125.000",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 5,
-                child: Container(
-                  margin: EdgeInsets.only(right: 5, top: 5),
-                  padding: EdgeInsets.all(10),
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color(0xff161637),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                            color: Color(0xff43D0A1),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Icon(
-                          FontAwesomeIcons.building,
-                          color: Color(0xff161637),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10),
-                        child: Text(
-                          "Building",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 5),
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "125.000",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Color(0xff161637),
               ),
-              Expanded(
-                flex: 5,
-                child: Container(
-                  margin: EdgeInsets.only(left: 5, top: 5),
-                  padding: EdgeInsets.all(10),
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color(0xff161637),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Color(0xff459CEE),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Icon(
+                      FontAwesomeIcons.tshirt,
+                      color: Color(0xff161637),
+                    ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                            color: Color(0xffFF5C58),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Icon(
-                          FontAwesomeIcons.robot,
-                          color: Color(0xff161637),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10),
-                        child: Text(
-                          "Tertiary",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 5),
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "75.000",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Bahnschrift",
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                      )
-                    ],
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Cloth",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.normal),
+                    ),
                   ),
-                ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "165.000",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-        ],
-      ),
-    );
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Color(0xff161637),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Color(0xff43D0A1),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Icon(
+                      FontAwesomeIcons.building,
+                      color: Color(0xff161637),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Building",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "125.000",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Color(0xff161637),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Color(0xffFF5C58),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Icon(
+                      FontAwesomeIcons.robot,
+                      color: Color(0xff161637),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Tertiary",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "75.000",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Bahnschrift",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
